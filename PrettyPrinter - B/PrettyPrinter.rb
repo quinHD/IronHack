10e35aabb5af7eb03488b2e8b2f8e3c195d715e8
SrLombarda <vavoom02@gmail.com>
2014-11-03 22:25:21 +0100

    Pretty Printer
    Recursivity + all kinds of data but hash

--- PrettyPrinter - B/PrettyPrinter.rb
+++ PrettyPrinter - B/PrettyPrinter.rb
@@ -1,6 +1,7 @@
-class PrettyPrinter
+require 'date'
 
-	def pretty_print array
+class PrettyPrinter
+	def print array
 		string_to_print = "Array:\n"
 		deep = 1
 		create_string_to_print array, string_to_print, deep
@@ -16,9 +17,21 @@ class PrettyPrinter
 				create_string_to_print item, string_to_print, deep
 				deep -=1	
 
-			else
+			elsif item.is_a? Date
+				string_to_print << "\t"*deep
+				string_to_print << item.to_s
+				string_to_print << "\n"
+			elsif item.is_a? (Hash)
+				string_to_print << "\n"
+			
+			elsif
+			   (item.is_a? Integer) || (item.is_a? String)
 				string_to_print << "\t"*deep
 				string_to_print << "#{item.to_s}"
+				string_to_print << "\n"
+			else
+				string_to_print << "\t"*deep
+				string_to_print << "An object I don't know how to print"
 				string_to_print << "\n"
 			end
 				
@@ -27,29 +40,29 @@ class PrettyPrinter
 
 end
 
-#puts PrettyPrinter.new.pretty_print(["hola", 5,["nivel2", 34, "muy confidencial"], "hhh",[[8,7,8],7]])
-#puts PrettyPrinter.new.pretty_print([[1,2],[[3,4],5],6])
+#puts PrettyPrinter.new.print(["hola", 5,["nivel2", 34, "muy confidencial"], "hhh",[[8,7,8],7]])
+#puts PrettyPrinter.new.print([1,2,3,["A","B"]])
+
+a_hash = {key: "value", other_key: "other_value"}
+a_date = Date.today
+
+
+#puts PrettyPrinter.new.print(["My", 3, a_date, ["Pied", "Piper"] ])
 
 RSpec.describe "Pretty Printer" do
 	it "print empty string" do
-		expect(PrettyPrinter.new.pretty_print([])).to eq("Array:\n")
+		expect(PrettyPrinter.new.print([])).to eq("Array:\n")
 	end
 end
 
 RSpec.describe "Pretty Printer" do
 	it "print one element" do
-		expect(PrettyPrinter.new.pretty_print([3])).to eq("Array:\n\t3\n")
+		expect(PrettyPrinter.new.print([3])).to eq("Array:\n\t3\n")
 	end
 end
 
 RSpec.describe "Pretty Printer" do
 	it "print two elements" do
-		expect(PrettyPrinter.new.pretty_print([3, "hola"])).to eq("Array:\n\t3\n\thola\n")
-	end
-end
-
-RSpec.describe "Pretty Printer" do
-	it "print two arrays" do
-		expect(PrettyPrinter.new.pretty_print("")
+		expect(PrettyPrinter.new.print([3, "hola"])).to eq("Array:\n\t3\n\thola\n")
 	end
 end
